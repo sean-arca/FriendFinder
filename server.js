8da0ed2f336ccadcfd1eefc97076803c71d8ba75
Sean Arca <sean.arca@northwestern.edu>
2018-03-03 16:37:39 -0600

    add middleware (body-parser)

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Middleware for parsing request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Listen to PORT
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
